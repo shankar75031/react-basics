50263ed9101545f585fbbe0229b2ae046f8dc718
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-03-27 23:48:31 +0530

    react hooks example

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import "./App.css";
 
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Person from "./Person/Person";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi i am react app</h1>
-        <Person name="Elsa" age="22" />
-        <Person name="Anna" age="21">
-          My Hobbies: Singing
-        </Person>
-      </div>
-    );
-  }
+export default function App(props) {
+  const [personsState, setPersonsState] = useState({
+    persons: [
+      {
+        name: "Anna",
+        age: 21,
+      },
+      {
+        name: "Elsa",
+        age: 22,
+      },
+    ],
+  });
+
+  const switchNameHandler = () => {
+    setPersonsState({
+      persons: [
+        {
+          name: "Sangeetha",
+          age: 21,
+        },
+        {
+          name: "Prabhashankar",
+          age: 22,
+        },
+      ],
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>Hi, I am React App</h1>
+      <button onClick={switchNameHandler}>Switch name</button>
+      <h1>Hi i am react app</h1>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      >
+        My Hobbies: Singing
+      </Person>
+    </div>
+  );
 }
