70d34af20ed14f4836ba92975626e6c0bfbde759
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-03-30 15:24:47 +0530

    added pseudo selector hover using styled-components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import "./App.css";
+import styled from "styled-components";
 
 import React, { Component } from "react";
 import Person from "./Person/Person";
 // import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
+
+const StyledButton = styled.button`
+  background-color: green;
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: lightgreen;
+    color: black;
+  }
+`;
 
 class App extends Component {
   state = {
@@ -117,9 +132,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I am React App</h1>
         <p className={classes.join(" ")}>This is really working</p>
-        <button style={style} onClick={this.togglePersonsHandler}>
+        <StyledButton onClick={this.togglePersonsHandler}>
           Toggle Persons
-        </button>
+        </StyledButton>
         {persons}
       </div>
     );
