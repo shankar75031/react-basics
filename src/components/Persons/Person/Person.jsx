6e742f71762b6c8cabde97f0f8f8b9a1a1651e19
Prabhashankar Kannapan <prabhashankarkannapan@gmail.com>
2021-04-07 00:10:54 +0530

    added react fragment

--- src/components/Persons/Person/Person.jsx
+++ src/components/Persons/Person/Person.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Aux from "../../../hoc/Auxillary";
 import classes from "./Person.module.css";
 
 class Person extends Component {
@@ -6,18 +7,20 @@ class Person extends Component {
     console.log("[Person.js] rendering");
 
     return (
-      <div className={classes.Person}>
-        <h1 onClick={this.props.click}>
-          I'm {this.props.name} and I am {this.props.age}
-        </h1>
-        <p>{this.props.children}</p>
-        <input
-          type="text"
-          name="name"
-          onChange={this.props.onNameChange}
-          value={this.props.name}
-        />
-      </div>
+      <React.Fragment>
+        <div className={classes.Person}>
+          <h1 onClick={this.props.click}>
+            I'm {this.props.name} and I am {this.props.age}
+          </h1>
+          <p>{this.props.children}</p>
+          <input
+            type="text"
+            name="name"
+            onChange={this.props.onNameChange}
+            value={this.props.name}
+          />
+        </div>
+      </React.Fragment>
     );
   }
 }
