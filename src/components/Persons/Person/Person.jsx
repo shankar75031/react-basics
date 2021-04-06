5bdc58ee0c360467689a474d3d4ad3cfa47fa4dc
Prabhashankar Kannapan <prabhashankarkannapan@gmail.com>
2021-04-07 01:28:32 +0530

    added prop types check

--- src/components/Persons/Person/Person.jsx
+++ src/components/Persons/Person/Person.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import Auxillary from "../../../hoc/Auxillary";
 import withClass from "../../../hoc/withClass";
 
@@ -24,5 +25,12 @@ class Person extends Component {
     );
   }
 }
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  onNameChange: PropTypes.func,
+};
 
 export default withClass(Person, classes.Person);
