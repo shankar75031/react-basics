9ad615e1b3070a61158c08344f1923e446564007
Prabhashankar Kannapan <prabhashankarkannapan@gmail.com>
2021-04-07 00:30:00 +0530

    passing props through higher order components

--- src/components/Persons/Person/Person.jsx
+++ src/components/Persons/Person/Person.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Auxillary from "../../../hoc/Auxillary";
+import withClass from "../../../hoc/withClass";
+
 import classes from "./Person.module.css";
 
 class Person extends Component {
@@ -8,21 +10,19 @@ class Person extends Component {
 
     return (
       <Auxillary>
-        <div className={classes.Person}>
-          <h1 onClick={this.props.click}>
-            I'm {this.props.name} and I am {this.props.age}
-          </h1>
-          <p>{this.props.children}</p>
-          <input
-            type="text"
-            name="name"
-            onChange={this.props.onNameChange}
-            value={this.props.name}
-          />
-        </div>
+        <h1 onClick={this.props.click}>
+          I'm {this.props.name} and I am {this.props.age}
+        </h1>
+        <p>{this.props.children}</p>
+        <input
+          type="text"
+          name="name"
+          onChange={this.props.onNameChange}
+          value={this.props.name}
+        />
       </Auxillary>
     );
   }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
--- src/hoc/WithClass.jsx
+++ src/hoc/WithClass.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function withClass(WrappedComponent, className) {
   return (props) => (
     <div className={className}>
-      <WrappedComponent />
+      <WrappedComponent {...props} />
     </div>
   );
 }
