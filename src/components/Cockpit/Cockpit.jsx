dd43d7051562436072a251e8271266b3f5c87982
Prabhashankar Kannapan <prabhashankarkannapan@gmail.com>
2021-04-06 23:21:31 +0530

    useEffect react hook

--- src/components/Cockpit/Cockpit.jsx
+++ src/components/Cockpit/Cockpit.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import classes from "./Cockpit.module.css";
 
 export default function Cockpit(props) {
+  useEffect(() => {
+    console.log("[Cockpit.js] useEffect");
+    setTimeout(() => {
+      alert("SAVED TO CLOUD");
+    }, 1000);
+  }, [props.persons]);
+
   let btnClass = "";
+  const assignedClasses = [];
+
   if (props.showPersons) {
     btnClass = classes.Red;
   }
 
-  const assignedClasses = [];
   if (props.persons.length <= 2) {
     assignedClasses.push(classes.red);
   }
